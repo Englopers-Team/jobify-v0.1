bcd0c246596f78e4606edfc539606d497ea410e8
messeili <mymoa.2014@gmail.com>
2020-09-13 15:31:26 +0300

    three routes for myjobs delete,update,myjob done by zatar and moh

--- functionality/company.js
+++ functionality/company.js
@@ -11,15 +11,43 @@ obj.companyJobs = (req, res) => {
 
     mainObj.client.query(SQL, Value).then((data) => {
         let id = data.rows[0].id;
+        let SQL1 = `SELECT * FROM company WHERE auth_id=${id};`
+        mainObj.client.query(SQL1).then((data1) => {
+            let SQL2 = `SELECT * FROM jobs WHERE company_id=${data1.rows[0].id};`
+            mainObj.client.query(SQL2).then((data2) => {
+                res.render('pages/company/myJobs', { data: data1.rows[0], data2: data2.rows })
+            })
+
+        })
+
 
     })
 }
 
 obj.companyDeleteJob = (req, res) => {
+    let jobID = req.params.jobID
+    let SQL = `DELETE FROM applications WHERE job_id=$1;`;
+    let Value = [jobID];
+    mainObj.client.query(SQL, Value).then(() => {
+        let SQL1 = `DELETE FROM jobs WHERE id=$1;`;
+        let Value2 = [jobID];
+        mainObj.client.query(SQL1, Value2).then(() => {
+            res.redirect('/company/jobs')
+
+        })
+
+    })
 
 }
 
 obj.companyUpdateJob = (req, res) => {
+    let jobID = req.params.jobID
+    let SQL = `UPDATE jobs SET title=$1 WHERE id=$2;`;
+    console.log(req.body.title);
+    let Value = [req.body.title, jobID];
+    mainObj.client.query(SQL, Value).then(() => {
+        res.redirect('/company/jobs')
+    })
 
 }
 
